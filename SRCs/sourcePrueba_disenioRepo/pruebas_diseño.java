e35eafeb81db14adca6f2aa7d123a896176256d8
angelaangela <angela.faundez@educa.madrid.org>
2025-11-15 21:01:24 +0100

    Diseño de carpetas2

---
+++ SRCs/sourcePrueba_disenioRepo/pruebas_diseño.java
@@ -0,0 +1,11 @@
+//archivo_prueba ... estoy diseñando la estructura de mi repo ...
+
+public class Prueba(){
+
+	public static void main (String[] args){
+		System.out.println("archivo_prueba ... estoy diseñando la estructura de mi repo ...");
+
+	}
+	
+
+}
